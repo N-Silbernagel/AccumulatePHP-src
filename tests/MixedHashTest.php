9be2695ade64ea48d74a621a10205837c9c5c7ea
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2022-10-12 22:35:49 +0200

    fix phpstan errors in MixedHashTest

--- tests/MixedHashTest.php
+++ tests/MixedHashTest.php
@@ -12,7 +12,7 @@ use Tests\Map\EqualHashable;
 final class MixedHashTest extends TestCase
 {
     /** @test */
-    public function it_should_compute_to_given_int()
+    public function it_should_compute_to_given_int(): void
     {
         $mixedHash = MixedHash::for(2294605);
 
@@ -20,7 +20,7 @@ final class MixedHashTest extends TestCase
     }
 
     /** @test */
-    public function it_should_compute_to_given_string()
+    public function it_should_compute_to_given_string(): void
     {
         $mixedHash = MixedHash::for('test');
 
@@ -28,7 +28,7 @@ final class MixedHashTest extends TestCase
     }
 
     /** @test */
-    public function it_should_throw_for_resource()
+    public function it_should_throw_for_resource(): void
     {
         $resource = fopen(__DIR__ . '/teststream.txt', 'r');
 
@@ -44,7 +44,7 @@ final class MixedHashTest extends TestCase
     }
 
     /** @test */
-    public function it_should_return_object_hash_for_object()
+    public function it_should_return_object_hash_for_object(): void
     {
         $obj = (object)[];
         $mixedHash = MixedHash::for($obj);
@@ -54,7 +54,7 @@ final class MixedHashTest extends TestCase
     }
 
     /** @test */
-    public function it_should_return_hashcode_for_hashable()
+    public function it_should_return_hashcode_for_hashable(): void
     {
         $hashable = new EqualHashable('hello');
         $mixedHash = MixedHash::for($hashable);
