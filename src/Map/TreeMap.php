ae3982f028129352b0efe64db56409c7f41399f3
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2022-10-19 20:30:36 +0200

    WIP add Treemap and TreeMapEntry

---
+++ src/Map/TreeMap.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AccumulatePHP\Map;
+
+use AccumulatePHP\Accumulation;
+
+final class TreeMap implements Map
+{
+
+    public static function new(): Accumulation
+    {
+        // TODO: Implement new() method.
+    }
+
+    public static function fromArray(array $array): Accumulation
+    {
+        // TODO: Implement fromArray() method.
+    }
+
+    public static function of(...$items): Accumulation
+    {
+        // TODO: Implement of() method.
+    }
+
+    public function toArray(): array
+    {
+        // TODO: Implement toArray() method.
+    }
+
+    public function put(mixed $key, mixed $value): mixed
+    {
+        // TODO: Implement put() method.
+    }
+
+    public function remove(mixed $key): mixed
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function get(mixed $key): mixed
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function isEmpty(): bool
+    {
+        // TODO: Implement isEmpty() method.
+    }
+
+    public function count(): int
+    {
+        // TODO: Implement count() method.
+    }
+
+    public function values(): Accumulation
+    {
+        // TODO: Implement values() method.
+    }
+
+    public static function fromAssoc(array $assocArray): ReadonlyMap
+    {
+        // TODO: Implement fromAssoc() method.
+    }
+
+    public function toAssoc(): array
+    {
+        // TODO: Implement toAssoc() method.
+    }
+}
